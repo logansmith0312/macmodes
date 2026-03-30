a648bdce4219771764d3b5e785bf2f3904541008
Logan Smith <logansmitty0312@gmail.com>
2026-03-29 20:22:11 -0600

    fixed petsc4py and slepc4py

---
+++ docs/petsc_reconfigure.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+  import sys
+  import os
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+  configure_options = [
+    '--download-fblaslapack',
+    '--download-mpich',
+    '--with-scalar-type=complex',
+    'PETSC_ARCH=arch-linux-c-debug',
+  ]
+  configure.petsc_configure(configure_options)
---
+++ docs/slepc_reconfigure.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+import os, sys
+exec(open(os.path.join('/home/logansmith/slepc','config','configure.py')).read())
